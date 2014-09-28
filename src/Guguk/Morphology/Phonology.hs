1ba1816d51367cd7ee40bc25ca7dd8e94c1111c3
Cumhur Korkut <cumhurkorkut@gmail.com>
2014-09-28 17:50:46 -0400

    Added some functions and tests to Morphology.Phonology

--- src/Guguk/Morphology/Phonology.hs
+++ src/Guguk/Morphology/Phonology.hs
@@ -1,2 +1,66 @@
 module Guguk.Morphology.Phonology where
 
+import qualified Data.Text as T
+
+import qualified Guguk.TurkishAlphabet as Alph
+import qualified Guguk.Phonetics as Ph
+
+same :: (Eq a) => [a] -> Bool
+same xs = and $ zipWith (==) xs (tail xs)
+
+-- Vowel harmony functions
+
+-- | Checks for the vowel harmony in the entire word.
+vowelHarmony :: T.Text -> Bool
+vowelHarmony t = same $ map Ph.vowelLocation vowelPhoneme
+  where vowelList = T.unpack $ T.filter Alph.isVowel t
+        vowelPhoneme = map (head . Alph.getPhonemes) vowelList
+
+{-|
+  A syllable's vowels are formed according to the last vowel
+  of the word it appends to. There is an A-Type harmony;
+  if last vowel of the word is a back vowel (a, ı, o, u), a is produced.
+  If last vowel is a frontal vowel (e, i, ö, ü), e is produced.
+  For plural suffix [-lAr] vowel is formed as these; kalem-ler, kitap-lar.
+-}
+aTypeHarmony :: T.Text -> Char
+aTypeHarmony = undefined
+
+{-|
+  There is also an I-Type harmony;
+  if last vowel is back and unrounded (a, ı), ı is produced.
+  if it is back and rounded (o,u), u is produced.
+  if it is frontal and unrounded (e, i), i is produced
+  and lastly if it is frontal and rounded (ö, ü), ü is produced.
+  For genitive suffix [-(n)In] eight outcomes are possible,
+    four of them: kalem-in, kitab-ın, okul-un, üzüm-ün.
+-}
+iTypeHarmony :: T.Text -> Char
+iTypeHarmony = undefined
+
+
+-- Consonant alteration functions
+
+{-|
+  When a suffix starting with some voiced consonants (c,d)
+  is appended to a word ending with a voiceless consonant (f,s,t,k,ç,ş,h,p)
+  first consonant of the suffix becomes voiceless (ç,t).
+  fıstık->fıstık-çı (not fıstık-cı), muzip->muzip-tir (not muzip-dir)
+
+  This function returns the two texts in a tuple,
+  with the first consonant alteration done if necessary.
+-}
+firstConsonantAlteration :: T.Text -> T.Text -> (T.Text, T.Text)
+firstConsonantAlteration w1 w2 = undefined
+
+{-|
+  If last letter of a word or suffix is a stop consonant (tr: süreksiz sert sessiz),
+  and a suffix that starts with a vowel is appended to that word,
+  last letter changes (voicing). Changes are p-b, ç-c, k-ğ, t-d, g-ğ.
+  kitap->kitab-a, pabuç->pabuc-u, çocuk->çocuğ-a, hasat->hasad-ı
+
+  This function returns the two texts in a tuple,
+  with the last consonant alteration done if necessary.
+-}
+lastConsonantAlteration :: T.Text -> T.Text -> (T.Text, T.Text)
+lastConsonantAlteration w1 w2 = undefined
---
+++ tests/MorphologyPhonology.hs
@@ -0,0 +1,66 @@
+module Main where
+
+import Guguk.Morphology.Phonology
+import Test.HUnit
+
+tests :: Test
+tests = TestList $ map TestCase
+  [
+    -- Vowel Harmony Tests
+    assertEqual "vowelHarmony \"hoşlanıyorum\"" True  (vowelHarmony "hoşlanıyorum")
+  , assertEqual "vowelHarmony \"doğruymuş\""    True  (vowelHarmony "doğruymuş")
+  , assertEqual "vowelHarmony \"sözlükte\""     True  (vowelHarmony "sözlükte")
+  , assertEqual "vowelHarmony \"vaziyet\""      False (vowelHarmony "vaziyet")
+  , assertEqual "vowelHarmony \"asistanlık\""   False (vowelHarmony "asistanlık")
+  , assertEqual "vowelHarmony \"strateji\""     False (vowelHarmony "strateji")
+
+  , assertEqual "aTypeHarmony \"parlamento\""   'a'   (aTypeHarmony "parlamento")
+  , assertEqual "aTypeHarmony \"yapımcı\""      'a'   (aTypeHarmony "yapımcı")
+  , assertEqual "aTypeHarmony \"anayasa\""      'a'   (aTypeHarmony "anayasa")
+  , assertEqual "aTypeHarmony \"teklifler\""    'e'   (aTypeHarmony "teklifler")
+  , assertEqual "aTypeHarmony \"senatör\""      'e'   (aTypeHarmony "senatör")
+  , assertEqual "aTypeHarmony \"örgüt\""        'e'   (aTypeHarmony "örgüt")
+
+  , assertEqual "iTypeHarmony \"anayasa\""      'ı'   (iTypeHarmony "anayasa")
+  , assertEqual "iTypeHarmony \"yapımcı\""      'ı'   (iTypeHarmony "yapımcı")
+  , assertEqual "iTypeHarmony \"parlamento\""   'u'   (iTypeHarmony "parlamento")
+  , assertEqual "iTypeHarmony \"okul\""         'u'   (iTypeHarmony "okul")
+  , assertEqual "iTypeHarmony \"teklifler\""    'i'   (iTypeHarmony "teklifler")
+  , assertEqual "iTypeHarmony \"özellik\""      'i'   (iTypeHarmony "özellik")
+  , assertEqual "iTypeHarmony \"senatör\""      'ü'   (iTypeHarmony "senatör")
+  , assertEqual "iTypeHarmony \"örgüt\""        'ü'   (iTypeHarmony "örgüt")
+
+  , assertEqual "firstConsonantAlteration \"fıstık\" \"cı\""
+    ("fıstık","çı") -- alteration happens
+    (firstConsonantAlteration "fıstık" "cı")
+  , assertEqual "firstConsonantAlteration \"muzip\" \"dir\""
+    ("muzip","tir") -- alteration happens
+    (firstConsonantAlteration "muzip" "dir")
+  , assertEqual "firstConsonantAlteration \"kolay\" \"dır\""
+    ("kolay","dır") --alteration doesn't happen because 'y' is not voiceless
+    (firstConsonantAlteration "kolay" "dır")
+  , assertEqual "firstConsonantAlteration \"kalıp\" \"lı\""
+    ("kalıp","lı")  --alteration doesn't happen because 'l' is not 'c' or 'd'
+    (firstConsonantAlteration "kalıp" "lı")
+
+  , assertEqual "lastConsonantAlteration \"kitap\" \"a\""
+    ("kitab","a")  -- alteration happens
+    (lastConsonantAlteration "kitap" "a")
+  , assertEqual "lastConsonantAlteration \"çocuk\" \"u\""
+    ("çocuğ","u")  -- alteration happens
+    (lastConsonantAlteration "çocuk" "u")
+  , assertEqual "lastConsonantAlteration \"çocuk\" \"lu\""
+    ("çocuk","lu") -- alteration doesn't happen because 'l' is not a vowel
+    (lastConsonantAlteration "çocuğ" "u")
+  , assertEqual "lastConsonantAlteration \"sac\" \"ı\""
+    ("sac","ı")    -- alteration doesn't happen because 'c' is not a stop consonant
+    (lastConsonantAlteration "sac" "ı")
+  ]
+
+runTests ::  IO ()
+runTests = do
+  _ <- runTestTT tests
+  return ()
+
+main :: IO ()
+main = runTests
